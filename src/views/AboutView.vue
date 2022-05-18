267e0cfee9e8175ea42e8c444d5d3f794f40312f
Fran Luque <franluque2@gmail.com>
2022-05-18 00:43:33 -0300

    Changed about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This was a website made by Bigmen, to aid the KIngdomsMC mods in hosting Kingdoms' Dungeon Run, a Roguelike Yugioh! experience</h1>
   </div>
 </template>
