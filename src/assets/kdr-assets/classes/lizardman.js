68371d508cbbc1dfdd304bb09d00e8e9d0bc2806
JustBryant <48495278+JustBryant@users.noreply.github.com>
2022-08-30 23:27:37 +0100

    Update lizardman.js

--- src/assets/kdr-assets/classes/lizardman.js
+++ src/assets/kdr-assets/classes/lizardman.js
@@ -82,6 +82,7 @@ export default {
     loot_mid: [
         'Unexpected Dai',
         'Night Beam',
+        'Cosmic Cyclone',
         'Magic Cylinder',
         'Trap Hole',
         'Scrap Iron Scarecrow',
@@ -126,14 +127,11 @@ export default {
 
 ],
     loot_high: [
-       'Wall of Disruption',
 'Treeborn Frog',
 'Chaos Sorcerer',
 'Instant Fusion / Elder Entity Norden',
 'Stardust Dragon',
 'Decode Talker',
-'Knightmare Phoenix',
-'Knightmare Cerberus',
 'Saryuja Skull Dread',
 'Brute Enforcer',
 'Destrudo, the Lost Dragon’s Frisson',
@@ -152,7 +150,7 @@ export default {
 'Reptilianne Vaskii',
 'Cosmic Fortress Gol’Gar / Alien Ammonite',
 'Reptilianne Ramifications / Reptilianne Scylla / Reptilianne Poison',
-'Alien Brain',
+'Viper\'s Grudge',
 'Therions’ “Empress” Arasia',
 'Reptilianne Recoil / Earthbound Immortal Ccarayhua',
 
