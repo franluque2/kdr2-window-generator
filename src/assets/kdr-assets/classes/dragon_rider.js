ee83c0c7a05c65f17aa69bb267a8ead8c91f97a0
JustBryant <48495278+JustBryant@users.noreply.github.com>
2022-10-30 22:40:01 +0000

    Update dragon_rider.js

--- src/assets/kdr-assets/classes/dragon_rider.js
+++ src/assets/kdr-assets/classes/dragon_rider.js
@@ -53,7 +53,7 @@ export default {
 'Book of Taiyou / A/D Changer',
 'Widespread Dud / Storm',
 'Dust Tornado / Tribute to the Doomed / Bait Doll',
-'Sphere Kuriboh / Rush Recklessly'
+'Sphere Kuriboh / Rush Recklessly',
 'Decoy Dragon',
 'Totem Dragon',
 'Kidmodo Dragon',
