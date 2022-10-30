dca6bab700b29be13cae7be8b4608bd36ca82eaa
JustBryant <48495278+JustBryant@users.noreply.github.com>
2022-10-30 22:39:43 +0000

    Update artificer.js

--- src/assets/kdr-assets/classes/artificer.js
+++ src/assets/kdr-assets/classes/artificer.js
@@ -53,7 +53,7 @@ export default {
 'Book of Taiyou / A/D Changer',
 'Widespread Dud / Storm',
 'Dust Tornado / Tribute to the Doomed / Bait Doll',
-'Sphere Kuriboh / Rush Recklessly'
+'Sphere Kuriboh / Rush Recklessly',
 'Machina Cannon',
 'Machina Defender / Machina Sniper / Commander Covington / Machina Force',
 'Ancient Gear Gadjiltron  Dragon / Ancient Gear Gadget / Ancient Gear Wyvern',
