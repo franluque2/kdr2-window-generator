592da1ae7966e03d0976399e719e71393e4c8d1d
JustBryant <48495278+JustBryant@users.noreply.github.com>
2022-10-30 23:16:10 +0000

    Update miner.js

--- src/assets/kdr-assets/classes/miner.js
+++ src/assets/kdr-assets/classes/miner.js
@@ -120,7 +120,7 @@ export default {
 'Number 101: Silent Honor ARK',
 'Effect Veiler',
 'Enemy Controller / Book of Moon',
-'Pot of Avarice / Pot of Duality'
+'Pot of Avarice / Pot of Duality',
 'Ice Hand / Fire Hand / Thunder Hand',
 'Cyber Jar',
 'Raigeki Break',
