6ec6eca064f748fb18da05596028f3ef50782bc5
Francisco Javier Luque Sugrañes <42848860+franluque2@users.noreply.github.com>
2022-12-03 14:55:20 -0300

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-img src="/ElectrumiteSplashScreen.jpg" dark><span class="black--text font-weight-black align-content-center">WE MUST FOREVER SUFFER FOR THE SINS OF PEPE (use the tool bar at the top left to begin)</span></v-img>
+    <v-img src="/btp.jpg" dark><span class="black--text font-weight-black align-content-center">WE MUST FOREVER SUFFER FOR THE SINS OF PEPE (use the tool bar at the top left to begin)</span></v-img>
 </template>
 
 <script>
