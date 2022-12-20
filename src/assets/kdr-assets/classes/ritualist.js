333906edfd25725c95b577429d75e396ed299de1
JustBryant <48495278+JustBryant@users.noreply.github.com>
2022-12-20 02:29:46 +0000

    Update ritualist.js

--- src/assets/kdr-assets/classes/ritualist.js
+++ src/assets/kdr-assets/classes/ritualist.js
@@ -15,13 +15,13 @@ export default {
 'Vylon Prism / Mist Wurm',
 'Wattdragonfly / Wattcobra / Wattkeeper / Wattkiwi / Watthydra',
 'Wattrain / Wattmole / Wattsquirrel / Wattgiraffe / Wattcastle',
-'Wattwoodpecker / Wattpheasant / Wattlemur'
+'Wattwoodpecker / Wattpheasant / Wattlemur',
 'Wattkinetic Puppeteer',
 'Wattsychic Fighter',
 'Batteryman AAA',
 'Blue Thunder T-45 / What Powers Machines?: All your Machine monsters are also treated as Thunder monsters.',
 'Ally of Justice Thunder Armor',
-'Morphtronic Magnen / Morphtronic Magnen Bar / Morphtronic Radion / Morphtronic Remoten / Morphtronic Earfon / Morphtronic Smartfon / Morphtronic Celfon'
+'Morphtronic Magnen / Morphtronic Magnen Bar / Morphtronic Radion / Morphtronic Remoten / Morphtronic Earfon / Morphtronic Smartfon / Morphtronic Celfon',
 'Morphtronic Vacuumen',
 'Gigathunder Giclops',
 'Kazejin / Suijin / Gate Guardian / Guardian of the Gates: (Once per turn): You can banish 1 “Kazejin”, 1 “Suijin”, and 1 “Sanga the Thunder” from your hand or GY; Special Summon “Gate Guardian” from your hand or GY (as if by its own effect). If a “Gate Guardian” in your control leaves the field, you can target 1 “Sanga the Thunder” that is banished or in your GY; Special Summon it.',
@@ -32,13 +32,13 @@ export default {
 ],
     loot_mid: [
 'Thunder Sea Horse',
-'The Creator / The Creator Incarnate'
+'The Creator / The Creator Incarnate',
 'Thunder Dragonmatrix / Thunder Dragondark',
 'Batteryman Charger / Batteryman AA / Batteryman D / Super-Electromagnetic Voltech Dragon',
 'Elemental HERO Sparkman / Elemental HERO Clayman / Elemental HERO Voltic / Thunder Giant Training: (Once per turn): While you control two or more Thunder monsters with different names, you can send one of them to the GY and discard 1 card, banish 1 “Elemental HERO Sparkman” and 1 “Elemental HERO Clayman” from your hand or Deck, then Special Summon 1 “Elemental HERO Thunder Giant” from your Extra Deck (this is treated as a Fusion Summon).',
 'Dark Beckoning Beast / Dark Summoning Beast / Hamon, Lord of Striking Thunder',
 'The Ascended of Thunder',
-'Photon Sanctuary'
+'Photon Sanctuary',
 'Thunderclap Monk',
 'Ready Fusion / Kaminari Attack',
 'Thunder Dragon Discharge',
