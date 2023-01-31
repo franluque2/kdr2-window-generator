b79a86ecb02b6ea7c0a33d19d7e6261a067a0c6e
JustBryant <48495278+JustBryant@users.noreply.github.com>
2023-01-31 22:53:53 +0000

    Update skills.js

--- src/assets/kdr-assets/skills.js
+++ src/assets/kdr-assets/skills.js
@@ -5,19 +5,19 @@ export default {
         'Chicken- (Once per Duel) Pay 500 LP: Draw 1 card. Your opponent can then pay 1000 LP to draw 1 card. Continue passing this choice with the LP cost doubling until one player decides to stop.',
 
 
-        'Mulligan - At the start of the duel, you can shuffle your hand back into your Deck and draw a new starting hand.',
+        'Mulligan (Stackable) - At the start of the duel, you can shuffle your hand back into your Deck and draw a new starting hand. Each new stack of this ability allows you to Mulligan an additional time.',
 
 
-        'Parasitic Invasion - Add 3 “Parasite Paracide” to your Deck (permanently). At the start of the Duel, activate the Flip effects of all “Parasite Paracide” in your Deck and Hand. Draw cards to replace the ones activated in your hand so that your hand size is 5 again. Your minimum deck size is 43.',
+        'Parasitic Invasion - Add 3 “Parasite Paracide” to your Deck (permanently). At the start of the Duel, activate the Flip effects of all “Parasite Paracide” in your Deck and Hand. Draw cards to replace the ones activated in your hand so that your hand size is 5 again.',
 
 
-        'Scapelamba- Start the game with a Token in play. (“Scapelamba” 1000 ATK/1000 DEF Level 4 Earth/Beast)',
+        'Scapelamba (Stackable) - Start the game with a Token in play (“Scapelamba” 2000 ATK/2000 DEF Level 4 Earth/Beast). Each stack of this skill grants you an additional token.',
 
 
-        'Staff of Avarice - (Once per Duel) You can shuffle 1 card in your GY back into the Deck to draw 1 card.',
+        'Staff of Avarice (Stackable) - (Once per Duel) You can shuffle 1 card in your GY back into the Deck to draw 1 card. Each stack of this skill allows you to activate it an additional time per duel.',
 
 
-        'Long Con - At the end of each round, get 1 Generic Low Quality Pool at random to add to your inventory (works the round you pick it).',
+        'Long Con (Stackable) - At the end of each round, get 1 Generic Low Quality Pool at random to add to your inventory. Each stack of this skill gives you a higher quality loot pool (ie Low -> Medium -> High). Max of 3 stacks.',
 
 
         'Gambler’s Gambit (During each End Phase) (Once per turn): You and your opponent do a dice battle. The winner draws 1 card.',
@@ -44,10 +44,10 @@ export default {
         'Card Vendor (During your first Standby Phase) (Once per Duel): You can activate up to 3 “Card Trader” from your Deck. You can activate “Card Trader” also during your opponent’s Standby Phase. (Add “Card Trader” to your inventory.)',
 
 
-        'Sacrifice to the Cards (Once per Duel) (Quick Effect): You can destroy 1 card you control and draw 2 cards. Your opponent can activate cards/effects in response to the activation of this skill.',
+        'Sacrifice to the Cards (Once per Duel) (Quick Effect) (Stackable): You can destroy 1 card you control and draw 2 cards. Your opponent can activate cards/effects in response to the activation of this skill. Each stack of this skill lets you activate this effect an additional time per Duel.',
 
 
-        'Metalmorph (Stackable) - Take 300 damage less from any source of damage.',
+        'Metalmorph (Stackable) - Take 300 damage less from any source of damage. Each stack of this skill increases the damage reduction by 100.',
 
 
         'Trade! - After each round, you can offer a singular player in your KDR a trade, so long as it’s equal from each loot type. (Inventory loot, treasure for treasure, skill for skill).',
@@ -74,19 +74,13 @@ export default {
         'Silver Tongue: Purchasing Training costs 1 less Gold.',
 
 
-        'Professional Duelist: If you win a match, gain 5 Gold extra instead of 1.',
+        'Professional Duelist (Stackable): If you win a match, gain 5 Gold extra instead of 1. Each stack increases the amount earned by 5 Gold.',
 
 
         'Pirate Soul - Whenever you get to choose a Treasure, you can choose 3 of the options presented.',
 
 
         'Thorns (Once per turn): After you take battle damage from a direct attack, you can inflict damage equal to half the amount you took to your opponent.',
-
-
-        'Cauterizer: Any LP gain (including CON) is halved.',
-
-
-        'Ice Pack: Any effect damage (including skills) is halved.',
 
 
         'Most Ambitious Crossover (Once per Run): Get a random Skill from KDR Classic (you do not get to choose).',
@@ -110,22 +104,22 @@ export default {
         'Spell Overflow! - At the end of your turn, gain LP equal to the number of Spell Cards in your GY x 300.',
 
 
-        'Digging for Treasure!: After you complete your Legendary Quest, excavate the top 5 cards of your Deck, and add 1 Treasure from among them to your hand. Shuffle the rest back into the Deck.',
+        'Digging for Treasure!: After you complete your Legendary Quest, excavate the top 10 cards of your Deck, and add 1 Treasure from among them to your hand. Shuffle the rest back into the Deck.',
 
 
-        'Normal Summon Alligator: When you Normal Summon “Alligator’s Sword” you can add 1 “Kunai with Chain” from your Deck or GY to your hand (Add “Alligator’s Sword” and “Kunai with Chain” to your inventory).',
+        'Normal Summon Alligator: When you Normal Summon “Alligator\'s Sword” you can add 1 “Kunai with Chain” from your Deck or GY to your hand, then draw 3 cards and discard 2 cards (Add “Alligator\'s Sword” and “Kunai with Chain” to your inventory).',
 
 
         'Faulty Programming: Each player draws 2 cards for their Draw Phase, except during the first Draw Phase. Each player must shuffle 1 card they control or in their hand into the Deck during their End Phase, except during the first End Phase.',
 
 
-        'Plundered Booty: You can permanently remove 1 Treasure from your inventory to gain 1 gold.',
+        'Plundered Booty: When you sell Treasure, you gain 3 Gold per Treasure instead of 1.',
 
 
         'Temporary Enhancements: During the Shop Phase, you can spend 1 Gold for 1 Stat Point (max of 3). Lose them at the end of the Round.',
 
 
-        'Chilly Shores (Once per Duel): Next turn, the next 2 times you would take damage, that damage becomes 0.',
+        'Chilly Shores (Once per Duel) (Stackable): Next turn, the next 2 times you would take damage, that damage becomes 0. Each stack of this ability lets you activate this an additional time per Duel.',
 
 
         'A Brilliant Birdy Boy (Once per Duel): If you activate a Normal Spell/Trap card, you can make the effect of that Spell/Trap the effect of a Normal Spell/Trap card in your GY instead.',
@@ -141,7 +135,7 @@ export default {
 
         'Last Stand: (Once per Run): If your Life Points are reduced to 0, you can set both players Life Points to 1000. You take no damage for the rest of the turn. During your next End Phase after using this Skill, you lose the Duel.',
 
-        'Tapped Potential: You can tribute 1 “Fiend Scorpion” to Special Summon 1 “Millennium Scorpion” from your Deck. “Millennium Scorpion” you control gain 1000 ATK for their effect instead of 500. (Add “Fiend Scorpion” and “Millennium Scorpion” to your inventory).',
+        'Tapped Potential: You can tribute 1 “Fiend Scorpion” to Special Summon 1 “Millennium Scorpion” from your Deck, then you can add up to 2 Trap Card(s) from your Deck to your hand. “Millennium Scorpion” you control gain 1000 ATK for their effect instead of 500. (Add “Fiend Scorpion” and “Millennium Scorpion” to your inventory).',
 
         'Archetype Madness: Declare 1 card name in your inventory and 1 archetype name. That card becomes part of that archetype for the rest of the run.',
 
@@ -149,9 +143,9 @@ export default {
 
         'Lifesteal: If your monster battles an opponent’s monster and inflicts damage to them, gain LP equal to the amount inflicted.',
 
-        'The Tragedy: If a monster you control is destroyed by battle you can add 1 “Tragoedia” from your Deck to your hand. (Add 1 “Tragoedia” to your Deck permanently.)',
+        'The Tragedy: If a monster you control is destroyed by battle you can add 1 “Tragoedia” from your Deck to your hand, and if you do, you can add 1 monster from your GY to your hand. (Add 1 “Tragoedia” to your Deck permanently.)',
 
-        'Heavy Sack: Gain 10 gold at the end of this round, after you collect interest.',
+        'Heavy Sack: Gain 15 gold at the end of this round, after you collect interest.',
 
         'Daring Duelist: You can run a 25 card deck, but change your LP to 4000 after you apply CON.',
 
@@ -167,7 +161,7 @@ export default {
         
         'Shadow Game: At the end of their respective turn, each player will lose Life Points equal to 100 times the number of cards in their own Graveyard.',
 
-        'Magic Madness: (Once per Duel): Select 3 Spell Cards in your GY. Your opponent adds 1 of them at random to your hand.',
+        'Magic Madness: (Once per Duel): You can banish 3 Spell Cards face-down from your GY. Your opponent randomly picks one of them to add to your hand. This turn, if you activate a Spell, you can banish it face-down. (Once per Duel): You can make your opponent add 1 of your face-down banished cards to your hand at random, and if you do, discard 1 card. ',
 
         'Forceful Activation: (Once per Duel): Target 1 Set Spell/Trap card your opponent controls. Reveal it and force its activation. If the activation timing is incorrect, destroy it.',
         
@@ -177,9 +171,9 @@ export default {
 
         'Rainbow Energy: Once per Duel): You can declare 1 attribute. For the rest of the Duel, all monsters you control are also treated as that attribute.',
         
-        'Bingo Machine GO!! Token (Stackable) - Gain 6 Treasures!',
+        'Bingo Machine GO!! Token (Stackable): You can pick this skill any number of times. When you gain this skill, gain 6 Treasures! Each stack gives you an additional Treasure each time you pick Bingo Machine GO!! Token.',
         
-        'Tank-Up: (Once per Duel) (During your Main Phase 1): You can switch all monsters you control to DEF position, then, they gain 1500 DEF.',
+        'Tank-Up: (Once per Duel) (During your Main Phase 1) (Stackable): You can switch all monsters you control to DEF position, then, they gain 1500 DEF. Each stack of this skill lets you activate this an additional time per Duel.',
         
         'Red Herring: (Once per Duel): You can select 1 monster you control. Your opponent must target monsters with that name for attacks.',
         
@@ -197,22 +191,28 @@ export default {
         
         'Surface Burial (Once per Duel) (During the first Draw Phase): You can send 1 monster from your Deck to the GY. Your opponent can then repeat this effect, but sends twice as many as the previous sent. Continue passing this choice with the number sent doubling until one player decides to stop.',
         
-        'Minigun (Once per turn): You start the game with 500 charges. You can spend any number of charges and lower the ATK 1 face-up monster by that much. If this reduces its ATK to 0, destroy the monster and gain 1000 charges. (Once per turn) Quick Effect: You can destroy 1 monster you control to gain 500 charges. Your opponent can activate cards/effects in response to the activation of this skill.',
+        'Minigun (Once per turn) (Stackable): You start the Duel with 500 charges. You can spend any number of charges and lower the ATK 1 face-up monster by that much. If this reduces its ATK to 0, destroy the monster and gain 1000 charges. (Once per turn) (Quick Effect): You can destroy 1 monster you control to gain 1000 charges. This effect can go on chain and is treated as a card effect. Each stack of this skill gives an additional 500 charges at the start of the Duel.',
         
         'Adventure of Child: You can Normal Summon/Set monsters of any level. You cannot Tribute Summon unless a card specifies for its effect to tribute a certain number of monsters (e.g. "Gilford the Lightning"). You may only activate 1 Spell card per turn, 1 Trap card per turn, and only set 1 Spell/Trap card per turn.',
         
         'Oops! All Generic: Remove all Class Loot from your inventory and gain 1 gold for every card removed. You may only purchase Generic Loot. Generic Loot costs half its normal price rounded up.',
         
-        'Tax Evasion: You do not pay tax on Loot.',
-        
-        'Tax Return: You can buy specifically the listed cards that have tax on it for the cost of the tax instead of the whole loot pool (reroll the original loot pool after).',
-        
         'Gamble your Tax Return: You gain gold equal to the tax rolled on random loot pools you acquire.',
 
-        'Holey Unholy Roly Poly Grounds: You can Normal Summon/Set thrice per turn, but you can’t Special Summon monsters.',
+        'Holey Unholy Roly Poly Grounds (Stackable): You can Normal Summon/Set thrice per turn, but you can’t Special Summon monsters. Each stack of this skill gives you an additional Normal Summon/Set per turn.',
         
         'Maximum “Card” Cramming: If your opponent Special Summons a monster, draw 1 card. You do not discard for hand size. You cannot Special Summon.',
         
         'Battle Boogy: You can switch the position of monsters you control the turn they are Summoned (not set), or after they’ve battled.',
+        
+        'Fusion Festival!: (Once per turn): If you Fusion Summon a monster, you can add 1 “Polymerization” and 1 monster used as Fusion Material for that monster from your GY to your hand (add “Polymerization” and “Garura, Wings of Resonant Life” to your inventory permanently). ',
+        
+        'Farmville (Stackable): You can only obtain this skill through the Shop Phase a single time. If you destroy an opponent’s monster by battle, gain a stack of this skill. This skill starts with 1 stack. Each stack of this skill gives you 100LP at the start of the duel. If you gain a stack of this skill, gain 100 LP.',
+        
+        'Giant Food (Stackable): You can only obtain this skill through the Shop Phase a single time. If you destroy an opponent’s monster by battle, gain a stack of this skill. (Once per turn): You can reveal 1 monster in your hand; spend any number of stacks (min 1) and reduce or increase that card\'s level by the amount spent and treat it as a Tuner until the end of the turn.',
+        
+        'Snacks for your Stacks (Once per Run): You can gain 5 Stacks on 1 Stackable skill, or, you can gain 1 random stackable skill. (KDR-ADMIN ONLY: Just roll until you hit a stackable skill, because the number of stackable skills may change a lot)',
+        
+        'Normie Gathering (Once per Duel) (Stackable): You can add 1 non-effect monster from your Deck to your hand, except for your Legendary Monster(s). (Once per Duel): You can Special Summon 1 Level 4 or lower non-effect monster from your hand. Each stack of this skill lets you add up to 2 additional non-effect monster(s) from your Deck to your hand.',
     ]
 }
