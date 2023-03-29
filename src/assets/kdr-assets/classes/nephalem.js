401ea30dbcbef7b1f9d3ef23703fe5a78c041abd
JustBryant <48495278+JustBryant@users.noreply.github.com>
2023-03-29 15:58:18 +0100

    Update nephalem.js

--- src/assets/kdr-assets/classes/nephalem.js
+++ src/assets/kdr-assets/classes/nephalem.js
@@ -47,8 +47,8 @@ export default {
 'Jain, Twilightsworn General / Ryko, Lightsworn Hunter',
 'Lumina, Lightsworn Summoner / Lumina, Twilightsworn Shaman',
 'Chaos Form / Magician of Black Chaos MAX',
-'Chaos Control (Once per turn): If you banish a LIGHT and a DARK monster: you can add 1 Trap Card from your Deck to your hand, then add 1 banished monster with the same Attribute as your current form (Darkform = Dark, Lightform = Light). You can only Special Summon five times in a turn.',
-'Entropy Generator (Once per turn) (During the End Phase): You can send the top card of your Deck to the GY, then you can banish 1 LIGHT and 1 DARK monster from your GY to draw 2 cards, discard 1 card.',
+'Chaos Control (Once per turn): If you banish a LIGHT and a DARK monster: you can add 1 Trap Card from your Deck to your hand, then add 1 banished monster with the same Attribute as your current form (Darkform = Dark, Lightform = Light). You can only Special Summon five times per turn.',
+'Entropy Generator (Once per turn) (During your End Phase): You can send the top card of your Deck to the GY, then you can banish 1 LIGHT and 1 DARK monster from your GY to draw 2 cards, discard 2 cards. / Tax +1',
 'Elementsaber Lapauila / Elementsaber Malo / Elementsaber Nalu / Palace of the Elemental Lords',
 'Elementsaber Molehu / Elementsaber Makani / Elementsaber Aina / Elemental Training',
 'Illusion of Chaos / Magicians’ Souls',
@@ -64,8 +64,8 @@ export default {
 'Incredible Ecclesia, the Virtuous / Fallen of Albaz / Despian Luluwalilith',
 'Mental Tuner / Chaos Ruler, the Chaotic Magical Dragon',
 'Manadome Riumheart / Manadome Hearless / Manadome Meek / Manadome Prime-Heart',
-'Chaos Mirage Dragon / Mirage Dragon / Dual Mirage: You can send 1 "Mirage Dragon" from your Deck to the GY to add 1 "Chaos Mirage Dragon" from your Deck to your hand. "Chaos Mirage Dragon" cannot activate its effect in the GY. This cannot leave your inventory by any means.',
-'Bystial Magnamhut / Bystial Saronir / Best Isles: The effects of “Bystial” monsters can only target monsters in your GY. This cannot leave your inventory by any means.',
+'Chaos Mirage Dragon / Mirage Dragon / Dual Mirage: You can send 1 "Mirage Dragon" from your Deck to the GY to add 1 "Chaos Mirage Dragon" from your Deck to your hand. "Chaos Mirage Dragon" cannot activate its effect in the GY. This Skill cannot leave your inventory.',
+'Bystial Magnamhut / Bystial Saronir / Best Isles: The effects of “Bystial” monsters can only target monsters in your GY. This Skill cannot leave your inventory.', 
 ]
 
 }
