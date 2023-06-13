0ff145bdd02c50553608a338f391582bb9b9e035
Fran Luque <franluque2@gmail.com>
2023-06-13 19:07:43 -0300

    change the order of the templates to standardize

--- src/assets/kdr-assets/response-templates-index.js
+++ src/assets/kdr-assets/response-templates-index.js
@@ -1,5 +1,5 @@
 import pretty from './response-templates/pretty-template'
 import bigmen from './response-templates/player-discord-templates/bigmens-response-template'
 export default{
-    templates:[pretty,bigmen]
+    templates:[bigmen,pretty]
 };
