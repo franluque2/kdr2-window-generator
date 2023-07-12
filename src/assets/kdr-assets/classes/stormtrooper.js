1048fc8b7e6ea69a41521f259c6fd9d209b757ea
JustBryant <48495278+JustBryant@users.noreply.github.com>
2023-07-12 18:21:23 +0100

    Update stormtrooper.js

--- src/assets/kdr-assets/classes/stormtrooper.js
+++ src/assets/kdr-assets/classes/stormtrooper.js
@@ -37,7 +37,7 @@ export default {
 'Yokotuner / Cupid Pitch',
 ],
     loot_mid: [
-'Terraforming / Pseudo-Space / Kozmotown / Kozmo Sliprider / Kozmo Tincan',
+'Crystron Phoenix / Crystron Citree / Crystron Thystvern / Crystron Sulfefnir / Crystron Smiger / Crystron Prasiortle / Clipped Wings: The effect of “Crystron Phoenix” when it's Synchro Summoned banishes 1 Spell/Trap card on the field and in the GY instead. This Skill cannot leave the inventory.',
 'B.E.S. Big Core MK-2 / Boss Rush / B.E.F. Zelos / Boss Enough for KDR: “B.E.S. monsters must lose 2 counters for their effect if they are able to, instead of 1. This Skill cannot leave your inventory.',
 'B.E.S. Big Core / B.E.S. Crystal Core / B.E.S. Tetran / Boss Rush / B.E.F. Zelos / Boss Enough for KDR: “B.E.S. monsters must lose 2 counters for their effect if they are able to, instead of 1. This Skill cannot leave your inventory.',
 'Synchron Explorer / Quickdraw Synchron / Unknown Synchron / Road Warrior / Nitro Warrior / Drill Warrior',
@@ -54,7 +54,7 @@ export default {
 'Kozmo Strawman / Kozmo Delta Shuttle',
 ],
     loot_high: [
-'Starry Knight Astel / Starry Knight Arrival / Starry Night, Starry Dragon',
+'Starry Knight Astel / Starry Knight Arrival / Starry Night, Starry Dragon,'
 'Jet Synchron / Jet Warrior',
 'Stardust Synchron / Stardust Trail / Stardust Illumination / Stardust Charge Warrior',
 'Kozmo Dark Destroyer / Kozmo Soartroopers / Level Eater',
@@ -62,7 +62,7 @@ export default {
 'Shooting Quasar Dragon / Formula Synchron / Righty Driver / Lefty Driver',
 'Revolution Synchron / Doppelwarrior / Tuning / Ancient Fairy Dragon / Starlight Junktion / Synchro World',
 'Crystron Halqifibrax / Crystron Quan / Crystron Entry / Crystron Quandax / Crystron Ametrix / Crystron Time-Space Drive: “Crystron Halqifibrax” must be Link Summoned using at least 1 “Crystron” monster. Also, “Crystron Halqifibrax” is treated as a Machine-Type Synchro Monster for the effects of the “Crystron” monsters. This Skill cannot leave the inventory.',
-'Crystron Phoenix / Crystron Citree / Crystron Thystvern / Crystron Sulfefnir / Crystron Smiger / Crystron Prasiortle / Clipped Wings: The effect of “Crystron Phoenix” when it\'s Synchro Summoned banishes 1 Spell/Trap card on the field and in the GY instead. This Skill cannot leave the inventory.',
+'Terraforming / Pseudo-Space / Kozmotown / Kozmo Sliprider / Kozmo Tincan',
 'Brionac, Dragon of the Ice Barrier / Dewloren, Tiger King of the Ice Barrier / Colossal Fighter',
 'Beckoned by the World Chalice / Chosen by the World Chalice / Crowned by the World Chalice / World Legacy - "World Chalice" / Ib the World Chalice Justiciar',
 'The Crimson Dragon / Synchro Over Top / Duelist Genesis',
